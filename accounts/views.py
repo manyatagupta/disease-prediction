c98f089bc38fb3d8797478fc281d6f32f372bb94
manyatagupta <manyatagupta1555@gmail.com>
2026-08-17 23:17:39 +0530

    Refactor: Convert authentication to Class-Based Views

--- accounts/views.py
+++ accounts/views.py
@@ -1,34 +1,40 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views import View
 
-def signup_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Registration successful. Welcome!')
-            return redirect('predictor:home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('predictor:home')
 
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = authenticate(username=form.cleaned_data.get('username'), password=form.cleaned_data.get('password'))
-            if user is not None:
-                login(request, user)
-                messages.success(request, f'You are now logged in as {user.username}.')
-                return redirect('predictor:home')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form': form})
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        messages.success(self.request, 'Registration successful. Welcome!')
+        return response
 
-def logout_view(request):
-    logout(request)
-    messages.info(request, 'You have successfully logged out.')
-    return redirect('predictor:home')
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    
+    def form_valid(self, form):
+        messages.success(self.request, f"You are now logged in as {form.get_user().username}.")
+        return super().form_valid(form)
+        
+    def get_success_url(self):
+        return reverse_lazy('predictor:home')
+
+class CustomLogoutView(View):
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        messages.info(request, 'You have successfully logged out.')
+        return redirect('predictor:home')
+        
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        messages.info(request, 'You have successfully logged out.')
+        return redirect('predictor:home')
